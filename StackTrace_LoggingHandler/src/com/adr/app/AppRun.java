bf1b7f379c8f1778309446ba89c86b36cfc2ca6a
Conexion <Conexion@uniroca.locall>
2024-10-02 17:26:42 -0500

    Commit primero, de sincronización

---
+++ StackTrace_LoggingHandler/src/com/adr/app/AppRun.java
@@ -0,0 +1,13 @@
+package com.adr.app;
+
+
+public class AppRun 
+{
+    
+    public static void main(String[] args) 
+    {
+        
+        System.out.println("Testing Run..");
+        
+    }
+}
